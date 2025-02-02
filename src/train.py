1bfe764b83691dda32f291455cc97a65afe07738
Yeda CUI <cuiyeda@outlook.com>
2025-02-02 21:54:40 +0800

    update figures

--- src/train.py
+++ src/train.py
@@ -162,7 +162,6 @@ def train_pm(config):
     return bestmodel
 
 if __name__ == "__main__":
-
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
